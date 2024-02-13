30995a9def79eaee5b4302d09b667cd1db160aba
sgilb <32495429+sgilb@users.noreply.github.com>
2024-02-13 21:43:21 +0000

    Add game logic to play via console

--- script.js
+++ script.js
@@ -13,22 +13,64 @@ function GameBoard() {
 
   const getBoard = () => board;
 
-  const markCell = (player, row, column) => {
-    board[row][column] ||= player;
+  const markCell = (row, column, token) => {
+    board[row][column] ||= token;
   };
 
   const getCell = (row, column) => board[row][column];
 
-  const printBoard = () => {
-    const boardText = "";
-    for (let i = 0; i < board.length; i++) {
-      for (let j = 0; j < board[i].length; j++) {
-        boardText += getCell(row, column) + " ";
-      }
-      console.log(boardText);
-      boardText = "";
-    }
+  const printBoard = () => console.table(board);
+
+  return { getBoard, markCell, getCell, printBoard };
+}
+
+function GameController(
+  playerOneName = "Player One",
+  playerTwoName = "Player Two"
+) {
+  const board = GameBoard();
+
+  const players = [
+    {
+      name: playerOneName,
+      token: "X",
+    },
+    {
+      name: playerTwoName,
+      token: "O",
+    },
+  ];
+
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
   };
 
-  return { getBoard, markCell, printBoard };
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const playRound = (row, column) => {
+    board.markCell(row, column, getActivePlayer().token);
+
+    // TODO: Check for winner and handle with message
+
+    switchPlayerTurn();
+    printNewRound();
+  };
+
+  // Initial prompt to start play
+  printNewRound();
+
+  return {
+    playRound,
+    getActivePlayer,
+    getBoard: board.getBoard,
+  };
 }
+
+const game = GameController();
